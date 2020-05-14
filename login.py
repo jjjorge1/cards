5d3f779ee254e333ff30ec264e52ac5bff984e7c
jorgecastillo <jjjorge1@gmail.com>
2020-05-13 23:34:19 -0400

    log-in page

--- login.py
+++ login.py
@@ -8,13 +8,39 @@ db = SQLAlchemy(app)
 #class for making profile object
 class profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    
     username = db.Column(db.String(25), nullable=False)
     password = db.Column(db.String(25), nullable=False)
     
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        Name = request.form["username"]
+        Password = request.form["password"]
+        print("hello1")
+        if db.session.query(profile).filter_by(username=Name, password=Password).all():
+        
+            print("hello2")
+        
+            return render_template("user.html", user=Name)
+        else:
+            return render_template("login.html")
+
+    else:
+        return render_template("login.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signUp():
+    if request.method == "POST":
+       
+        newName = request.form["username"]
+        newPassword = request.form["password"]
+        newProfile = profile( username=newName, password=newPassword)
+        db.session.add(newProfile)
+        db.session.commit()
+        return redirect("/")
+    return render_template("signup.html")
 
 
 ##creating DB and running app
